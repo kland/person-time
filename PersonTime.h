a536ecc21fcbfe58b7b5d80192e6e1242cea1a52
Karl Landström <karl.landstrom@ki.se>
2013-04-12 17:42:08 +0200

    Improve documentation

--- PersonTime.h
+++ PersonTime.h
@@ -3,8 +3,8 @@
 
 void PersonTime_Init(double partition[], int n); /*defines the time interval partition for the person-time calculation. For example with n = 3 and partition = (60, 65, 70) the intervals are [0, 60), [60, 65) and [65, 70).*/
 
-void PersonTime_Add(double t1, double t2); /*adds time segment [t1, t2] to intervals*/
+void PersonTime_Add(double t1, double t2); /*adds person-time segment [t1, t2]*/
 
-void PersonTime_Print(void); /*prints array intervals*/
+void PersonTime_Print(void); /*prints total person-time for each interval to standard output*/
 
 #endif
