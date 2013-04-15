8513c0fc58d29f2a57b8474d2bbe1cf0079e083a
Karl Landström <karl.landstrom@ki.se>
2013-04-15 11:06:30 +0200

    Add missing file

---
+++ Test.c
@@ -0,0 +1,17 @@
+#include <float.h>
+#include "PersonTime.h"
+#include "Util.h"
+
+int main(void)
+{
+	int i;
+	double partition[] = {60, 65, 70, DBL_MAX};
+	double segments[][2] = {{65.1, 65.7}, {58.7, 62.1}};
+	
+	PersonTime_Init(partition, LEN(partition));
+	for (i = 0; i < LEN(segments); i++) {
+		PersonTime_Add(segments[i][0], segments[i][1]);
+	}
+	PersonTime_Print();
+	return 0;
+}
